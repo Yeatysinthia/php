528da8f7948e81c1acdc9080223ce6f98e9194e0
Yeatysinthia <you@example.com>
2026-03-31 13:02:42 +0600

    commit

---
+++ oop/static-keyword.php
@@ -0,0 +1,22 @@
+<?php
+class Demo {
+public static $name="hekdfasdgfdfdsgs";
+    const GREETING = "Hello, PHP World!<br>"; 
+
+    public static function sayHello() {       
+        echo "This is a static method<br>";
+    }
+    public static function sinthia() {       
+        echo "jkddddddddddddddddd". "<br>";
+    }
+}
+
+
+// Demo::sayHello();
+
+echo Demo::$name = "kjldhgyhdgfsd";
+
+
+
+// echo Demo::GREETING ;
+?>
